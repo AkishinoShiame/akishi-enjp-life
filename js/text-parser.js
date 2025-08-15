599db83bc08717933c48d4e011716c960a154994
akishinoshiame <akishinoshiame@DESKTOP-QUO2OEP.localdomain>
2025-08-16 02:29:56 +0900

    Update parser

--- js/text-parser.js
+++ js/text-parser.js
@@ -3,19 +3,18 @@
  */
 
 async function loadFileContent(url) {
-    fetch('url')
+    
+}
+
+function FillSelfInto() {
+    fetch("../src/raw-Section1")
     .then((response) => {
         console.log(response); 
-        return response.json();
     })
     .catch((error) => {
         console.log(`Error: ${error}`);
     })
-}
-
-function FillSelfInto() {
-    let MySection1 = loadFileContent("../src/raw-Section1");
-    alert(MySection1);
+    let MySection1 = response.json();
     document.getElementById("japanese-content").innerText = MySection1["#Japanese"];
     document.getElementById("english-content").innerText = MySection1["#English"];
 }
