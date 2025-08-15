b11443799b6babebe1e57a1010f11b43f8fd70ac
akishinoshiame <akishinoshiame@DESKTOP-QUO2OEP.localdomain>
2025-08-16 03:07:31 +0900

    add disallow robot

--- js/text-parser.js
+++ js/text-parser.js
@@ -2,22 +2,14 @@
  *  parser for content
  */
 
-async function loadFileContent(url) {
-    
-}
-
-function FillSelfInto() {
-    let MySection1;
-    fetch("./src/raw-Section1")
-    .then((response) => {
-        MySection1 = response.json();
-        console.log(MySection1); 
-    })
+async function FillSelfInto() {
+    let MySection1 = fetch("./src/raw-Section1")
+    .then((response) => response.json())
     .catch((error) => {
         console.log(`Error: ${error}`);
     })
-    document.getElementById("japanese-content").innerText = MySection1["#Japanese"];
-    document.getElementById("english-content").innerText = MySection1["#English"];
+    document.getElementById("japanese-content").innerText = await MySection1["#Japanese"];
+    document.getElementById("english-content").innerText = await MySection1["#English"];
 }
 
 addEventListener("DOMContentLoaded", (event) => {
